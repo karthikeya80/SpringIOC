92545356d2af81722e49827a06671b708b7eb7b6
karthikeya80 <karthikeya80@email.com>
2025-07-07 13:44:58 +0530

    Setter & const Injection -2

---
+++ src/main/java/com/setterinjection/Exam.java
@@ -0,0 +1,12 @@
+package com.setterinjection;
+
+public class Exam {
+
+	public static void main(String[] args) {
+
+		Student student = new Student();
+		student.setStudentName("Karthik P");
+		student.displayName();
+	}
+
+}
